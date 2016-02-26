4a474a359b0d0842ddc01a92da8c0290a7b64f25
William Thornton <wet13827@william-thorntons-macbook-pro.local>
2016-02-26 12:01:18 -0600

    Initial Commit

---
+++ MARS-iOS-app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MARS-iOS-app
+//
+//  Created by William Thornton on 2/26/16.
+//  Copyright © 2016 Padawan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
